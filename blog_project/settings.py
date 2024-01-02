28f01aef26687b618301450763b929770855c44c
Filbog <f.boguslawski101@gmail.com>
2024-01-02 16:11:09 +0100

    added .gitignore

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -131,3 +131,28 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # My settings.
 LOGIN_REDIRECT_URL = "blog_app:index"
 LOGOUT_REDIRECT_URL = "blog_app:index"
+
+# settings for Platform.sh remote server
+from platformshconfig import Config
+
+config = Config()
+if config.is_valid_platform():
+    ALLOWED_HOSTS.append(".platformsh.site")
+
+    if config.appDir:
+        STATIC_ROOT = Path(config.appDir) / "static"
+    if config.projectEntropy:
+        SECRET_KEY = config.projectEntropy
+
+    if not config.in_build():
+        db_settings = config.credentials("database")
+        DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.postgresql",
+                "NAME": db_settings["path"],
+                "USER": db_settings["username"],
+                "PASSWORD": db_settings["password"],
+                "HOST": db_settings["host"],
+                "PORT": db_settings["port"],
+            }
+        }
